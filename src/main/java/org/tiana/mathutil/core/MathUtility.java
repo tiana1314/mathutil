46c115ed4b59ff3067683140605fe93f7ecf0d29
tiana1314 <tuanhbinglongbp@gmail.com>
2025-10-20 11:34:45 +0700

    update main source by using recursion, to see green4ever

--- src/main/java/org/tiana/mathutil/core/MathUtility.java
+++ src/main/java/org/tiana/mathutil/core/MathUtility.java
@@ -12,18 +12,35 @@ public class MathUtility {
     // 0, 20 -> boundary - biên giới của tập giá trị
     //          xích qua 1 xíu là sang vùng invalid boundary!!!
     //0, xích nhẹ sang trái 1 đơn vị, ko ổn, ko tính đc, -1 sao tính !!!
-    public static long getFactorial(int n)  {
-            if(n<0 || n>20) {
-                //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về
-                throw new IllegalArgumentException("n must be between 0 and 20") ;
+//    public static long getFactorial(int n)  {
+//            if(n<0 || n>20) {
+//                //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về
+//                throw new IllegalArgumentException("n must be between 0 and 20") ;
+//
+//            }
+//            long result = 1;
+//            for (int i = 1; i <= n; i++) {
+//                result *= i;
+//            }
+//            return result;
+//
+//    }
+    //VIET HAM DUNG DE QUY
+    //5! = 5 * 4!
+    //4! = 4 * 3!
+    //3! = 3 * 2!
+    //2! = 2 * 1!
+    //1! = 1
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về
+            throw new IllegalArgumentException("n must be between 0 and 20");
 
-            }
-            long result = 1;
-            for (int i = 1; i <= n; i++) {
-                result *= i;
-            }
-            return result;
-
+        }
+        if (n == 0 || n == 1)
+            return 1;
+        return n * getFactorial(n - 1);
     }
+    //KIỂM THỬ LẠI CODE ĐÃ TỐI ƯU, -REGRESSION TESTING, TEST LẠI THỨ ĐÃ TỪNG TEST
 
 }
